7f3e594093a86c872b42dc16ada8ab58e820aef6
Maxime Bergeron <mazout360@gmail.com>
2013-04-19 13:09:00 -0400

    Hackfest support
    Change-Id: Icaa494b608199fb4869ce618ddb7eb1b00221961

--- p999.mk
+++ p999.mk
@@ -3,11 +3,8 @@ $(call inherit-product, device/lge/star-common/star.mk)
 # Inherit non-open-source blobs.
 $(call inherit-product-if-exists, vendor/lge/p999/p999-vendor.mk)
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/lge/p999/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
+TARGET_KERNEL_SOURCE := kernel/lge/star
+TARGET_KERNEL_CONFIG := p999_ICS_defconfig
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/p999/overlay
 
@@ -17,12 +14,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ueventd.rc:root/ueventd.rc \
     device/lge/p999/vold.fstab:system/etc/vold.fstab \
     device/lge/p999/gps.conf:system/etc/gps.conf \
-    device/lge/p999/fstab.p999:root/fstab.p999 \
     device/lge/p999/init.vsnet:system/bin/init.vsnet
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel \
-    $(LOCAL_PATH)/prebuilt/wireless.ko:system/lib/modules/wireless.ko
 
 PRODUCT_PACKAGES += \
     lights.p999 \
